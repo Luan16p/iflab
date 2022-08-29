c44930f28b558aeeaa878e0f53a0d3527f5c302e
Luan16p <luanpdev@gmail.com>
2022-08-29 02:13:54 -0400

    Atualização nas rotas designadas e estilo

--- Pages/Error/PageNotFound.tsx
+++ Pages/Error/PageNotFound.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
+  document.title = 'Page Not Found';
   return (
     <div>
       <main className="error">
@@ -11,6 +12,14 @@ export default function PageNotFound() {
             <h2>Error 404</h2>
 
             <p>A Pagina que você está procurando não existe ou foi alterada.</p>
+
+            <ul className="notFound--try">
+              <h3>Tente:</h3>
+
+              <li>Especificar melhor a Rota</li>
+              <li>Verificar o nome da pagina</li>
+              <li>Evitar caminhos ilegais</li>
+            </ul>
 
             <div className="btn-404">
               <Link to="/" className="btn-404">
